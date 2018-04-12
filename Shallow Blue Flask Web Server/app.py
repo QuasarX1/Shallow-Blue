d5744e8e6f649e16343258a34da8831b53f96240
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2018-04-12 12:37:26 +0100

    Added wraps from functools to allow url_for to work with decorated views.

--- Shallow Blue Flask Web Server/app.py
+++ Shallow Blue Flask Web Server/app.py
@@ -6,8 +6,11 @@ import random
 import string
 import WTFClasses
 
-# Creating the Flask object------------------------------------------------------------------------------------------------
+# Specific Imports---------------------------------------------------------------------------------------------------------
 from flask import Flask, render_template, redirect, url_for, session, flash
+from functools import wraps
+
+# Creating the Flask object------------------------------------------------------------------------------------------------
 app = Flask(__name__)
 
 # Adding the app config data
@@ -24,6 +27,7 @@ wsgi_app = app.wsgi_app
 
 # Custom Decorators---------------------------------------------------------------------------------------------------------
 def testLogin(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         loggedIn = False
 
