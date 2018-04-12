7b48bd53c9648af542aefde9520f4d2399f8c66b
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2018-04-12 15:45:41 +0100

    Removed the format argument in the first password field in the SignupForm as it shouldn't have been there.

--- Shallow Blue Flask Web Server/WTFClasses.py
+++ Shallow Blue Flask Web Server/WTFClasses.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
     usernameTextBox = StringField("Username", validators = [DataRequired(message = "This is a required field.")])
     passwordPasswordBox = PasswordField("Password", validators = [DataRequired(message = "This is a required field.")])
     submitButton = SubmitField("Login")
+
+class SignupForm(FlaskForm):
+    firstNameTextBox = StringField("Username", validators = [DataRequired(message = "This is a required field.")])
+    lastNameTextBox = StringField("Username", validators = [DataRequired(message = "This is a required field.")])
+    usernameTextBox = StringField("Username", validators = [DataRequired(message = "This is a required field.")])
+    emailTextBox = StringField("Username", validators = [DataRequired(message = "This is a required field."), Email("This field must contain a valid email adress.")])
+    dobDayIntegerBox = IntegerField("Day")
+    dobMonthIntegerBox = IntegerField("Month")
+    dobYearIntegerBox = IntegerField("Year")    
+    passwordPasswordBox = PasswordField("Password", validators = [DataRequired(message = "This is a required field.")])
+    repeatPasswordPasswordBox = PasswordField("Password", validators = [DataRequired(message = "This is a required field."), EqualTo(passwordPasswordBox, "Your passwords don't match. Please try entering them again.")])
+    submitButton = SubmitField("Signup")
