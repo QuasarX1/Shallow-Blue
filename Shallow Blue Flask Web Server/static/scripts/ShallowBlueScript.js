e7b670edcd0183f2bd14143535b0110980284b33
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2018-06-05 14:19:48 +0100

    Fixed menu bar transition isue.

--- Shallow Blue Flask Web Server/static/scripts/ShallowBlueScript.js
+++ Shallow Blue Flask Web Server/static/scripts/ShallowBlueScript.js
@@ -1,12 +1,20 @@
 /* Open when someone clicks on the span element */
 function openNav() {
     if ($(window).width() > 576) {
-        document.getElementById("myNav").style.display = "unset";
-        document.getElementById("myNav").style.width = "100%";
+        document.getElementById("myNav").style.height = "100%";
+        
+        setTimeout(function () {
+            document.getElementById("myNav").style.transition = "0.5s";
+            document.getElementById("myNav").style.width = "100%";
+        }, 1);
     }
     else {
-        document.getElementById("myNav").style.display = "unset";
-        document.getElementById("myNav").style.height = "100%";
+        document.getElementById("myNav").style.width = "100%";
+
+        setTimeout(function () {
+            document.getElementById("myNav").style.transition = "0.5s";
+            document.getElementById("myNav").style.height = "100%";
+        }, 1);
     }
     
 }
@@ -15,13 +23,19 @@ function openNav() {
 function closeNav() {
     if ($(window).width() > 576) {
         document.getElementById("myNav").style.width = "0%";
-
-        setTimeout(function () { document.getElementById("myNav").style.display = "none"; }, 500); 
+        
+        setTimeout(function () {
+            document.getElementById("myNav").style.transition = "unset";
+            document.getElementById("myNav").style.height = "0%";
+        }, 500);
     }
     else {
         document.getElementById("myNav").style.height = "0%";
-
-        setTimeout(function () { document.getElementById("myNav").style.display = "none"; }, 500);
+        
+        setTimeout(function () {
+            document.getElementById("myNav").style.transition = "unset";
+            document.getElementById("myNav").style.width = "0%";
+        }, 500);
     }
     
 } 
